7da0b4d2557e4b102f96e44a36b4978a94e1b62b
Vlad <vlad@bailescu.ro>
2020-02-03 15:40:10 +0200

    Updated install script
    See https://askubuntu.com/a/1207746/1040422

--- install-gitslave.sh
+++ install-gitslave.sh
@@ -2,4 +2,6 @@
 set -ex
 wget https://sourceforge.net/projects/gitslave/files/gitslave-2.0.2.tar.gz
 tar -xzvf gitslave-2.0.2.tar.gz
-cd gitslave-2.0.2 && make && sudo make install_sw
+cd gitslave-2.0.2
+sed 's/pod2man/pod2man --name gits/' -i Makefile
+make && sudo make install
