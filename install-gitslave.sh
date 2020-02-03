268a0a4d0483ea658f311cad2018415aca36e4a4
Vlad <vlad@bailescu.ro>
2020-02-03 15:42:22 +0200

    Update install-gitslave.sh

--- install-gitslave.sh
+++ install-gitslave.sh
@@ -4,4 +4,6 @@ wget https://sourceforge.net/projects/gitslave/files/gitslave-2.0.2.tar.gz
 tar -xzvf gitslave-2.0.2.tar.gz
 cd gitslave-2.0.2
 sed 's/pod2man/pod2man --name gits/' -i Makefile
-make && sudo make install
+sed 's/pod2man/pod2man --name gits-checkup/' -i contrib/Makefile
+make
+sudo make install
